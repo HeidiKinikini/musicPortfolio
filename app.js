805d0f26e1695131fd9e3f299123fdaadc866e39
HeidiKini <heididuckwitz@westernwyoming.edu>
2025-01-30 15:49:24 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const menu = document.getElementsByClassName("btn-toogle-nav");
+const menuLinks = document.getElementsbyId("btn-toggle-nav");
+
+let activeMenu = menu.addEventListener("onClick", function(){
+    window.alert("clicked");
+})
